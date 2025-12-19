7da441cd34e7295e306c7223ab64867d94e50c2f
Aldy <aldy64777@gmail.com>
2025-12-19 20:29:24 +0800

    ubah route home

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use App\Http\Controllers\DiagnosaController;
 use App\Http\Controllers\AturanController;
 use App\Http\Controllers\GejalaController;
 
-
+Route::get('/', function () {
+    // Arahkan langsung ke route bernama 'login'
+    return redirect()->route('login');
+});
 
 // Grup untuk route yang hanya bisa diakses oleh tamu (belum login)
 
